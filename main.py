ced5fc316bad05406a250256b4046f00908fb7e0
Razzynt <eubernardoassis>
2021-07-31 00:33:54 -0300

    reproduz texto utilizando uma interface

--- main.py
+++ main.py
@@ -15,17 +15,18 @@ repro.setProperty('voice', 'pt+m7')
 rate = repro.getProperty('rate')
 repro.setProperty('rate', rate-50)
 
-#função para reproduzir voz
-def voz(resposta):
-    repro.say(resposta)
-    repro.runAndWait()
-
 #definindo caixa de texto
 text = Text(app, height=5, width=53)
 text.place(x = 75, y =100 , width = 250, height = 80)
 
+#função para reproduzir voz
+def voz():
+    t = text.get("1.0", "end-1c")
+    repro.say(t)
+    repro.runAndWait()
 
 
-#text = str(input('Digite o texto: '))
-#voz(text)
+#definido botão
+button = Button(app, text = 'OK', command=voz)
+button.place(x = 174, y = 200)
 app.mainloop()

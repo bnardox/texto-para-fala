154e7f31d8a2c6bc4cc617dd06445e760b9d0416
Razzynt <eubernardoassis>
2021-07-31 00:00:17 -0300

    reproduzindo voz

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pyttsx3
 
+
+
 repro = pyttsx3.init()
 
 repro.setProperty('voice', 'pt+m7')
+
+rate = repro.getProperty('rate')
+repro.setProperty('rate', rate-50)
+
+def voz(resposta):
+    repro.say(resposta)
+    repro.runAndWait()
+
+text = str(input('Digite o texto: '))
+
+voz(text)
